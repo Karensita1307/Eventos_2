14c17031c1566f93822cfe469e78e35bb5934950
Karensita1307 <karen.sofia.lopez@correounivalle.edu.co>
2025-03-13 14:48:00 -0500

    Yasta

--- opp_intro/src/App.java
+++ opp_intro/src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         System.out.println("Hello, World!");
+
+        Humano humano1 = new Humano("Juan", "Perez", (byte) 25);
+            System.out.println(humano1.getName());
+
+
     }
 }
---
+++ opp_intro/src/Humano.java
@@ -0,0 +1,38 @@
+public class Humano {
+    
+    String name;
+    String lastname;
+    byte age;
+    
+    public Humano(String name, String lastname, byte age) {
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+
+}
